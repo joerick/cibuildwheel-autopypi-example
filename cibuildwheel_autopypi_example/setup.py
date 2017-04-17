5e321d1bbc184d7f7a044f72cf8ceb51814adb27
Joe Rickerby <joerick@mac.com>
2017-04-17 19:37:24 +0100

    Initial commit

---
+++ cibuildwheel_autopypi_example/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, Extension
+
+setup(
+    name="cibuildwheel_autopypi_example",
+    ext_modules=[Extension('cibuildwheel_autopypi_example', sources=['cibuildwheel_autopypi_example.c'])],
+    version="0.1.0",
+)
