ae8d3d34aa20905b06da4630a413eab502e1a11c
Joe Rickerby <joerick@mac.com>
2017-04-19 22:05:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, Extension
 setup(
     name="cibuildwheel_autopypi_example",
     ext_modules=[Extension('cibuildwheel_autopypi_example', sources=['cibuildwheel_autopypi_example.c'])],
-    version="0.1.1",
+    version="0.1.2",
 )
